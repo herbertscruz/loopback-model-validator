de688ac77ec0fe6aa6e3af4c11c2459235e4ef20
Herberts Cruz <herbertscruz@gmail.com>
2019-10-20 01:39:14 -0300

    Adding validator tests

--- test/validators-test.js
+++ test/validators-test.js
@@ -3,6 +3,10 @@ const server = require('./loopback/server/server');
 const request = require('supertest')(server);
 
 describe('mixins/validators.js', () => {
+  this.getUnique = () => {
+    if (!this.unique) this.unique = 0;
+    return ++this.unique;
+  };
   this.input = () => {
     return JSON.parse(JSON.stringify({
       "length": "ABC",
@@ -14,7 +18,7 @@ describe('mixins/validators.js', () => {
       "urlIp": "http://www.y.com",
       "cpf": "95466673072",
       "cnpj": "60525221000105",
-      "unique": "zv",
+      "unique": this.getUnique(),
       "inclusionOf": "A"
     }));
   };
@@ -26,6 +30,37 @@ describe('mixins/validators.js', () => {
       .send(input)
       .then(response => {
         assert.isOk(response);
+      });
+  });
+
+  it('should return with applied validators', async () => {
+    const data = this.input();
+    data.length = 'AB';
+    data.is = 'AB';
+    data.min = 'gh';
+    data.max = 'jklmnopqrst';
+    data.pattern = 'tuv';
+    data.email = 'w@x';
+    data.urlIp = 'http://www.y';
+    data.cpf = '95466673070';
+    data.cnpj = '60525221000100';
+    data.unique = data.unique - 1;
+    data.inclusionOf = 'C';
+    return request
+      .post('/api/TestLabs')
+      .send(data)
+      .then(response => {
+        assert.property(response.body.error.details.messages, 'length');
+        assert.property(response.body.error.details.messages, 'is');
+        assert.property(response.body.error.details.messages, 'min');
+        assert.property(response.body.error.details.messages, 'max');
+        assert.property(response.body.error.details.messages, 'pattern');
+        assert.property(response.body.error.details.messages, 'email');
+        assert.property(response.body.error.details.messages, 'urlIp');
+        assert.property(response.body.error.details.messages, 'cpf');
+        assert.property(response.body.error.details.messages, 'cnpj');
+        assert.property(response.body.error.details.messages, 'unique');
+        assert.property(response.body.error.details.messages, 'inclusionOf');
       });
   });
 });
